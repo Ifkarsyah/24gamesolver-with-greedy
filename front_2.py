dc3c57e63e5e8721ddfa2948411cf76451040665
Ferdian Ifkarsyah <ferdian.ifkarsyah@gmail.com>
2019-02-11 10:43:39 +0700

    add how to merge with frontend1

--- front_2.py
+++ front_2.py
@@ -13,3 +13,9 @@ def input_from_file():
 def output_to_file(expr):
     with open(sys.argv[2], "w") as fout:
         fout.write(expr)
+
+# CARA FRONT END 1 BUAT DIGABUNGIN
+# 1. AMBIL DATA raw_input DARI input_from_file()
+# 2. TampilinDiGUI(raw_input), format raw_input = [1,2,3,4]
+# 3. AMBIL DATA expr DARI output_from_file()
+# 4. TampilindiGUI(expr), format expr="1+2+3+4"
